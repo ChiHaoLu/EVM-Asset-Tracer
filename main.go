960857b844a0a7631431e109acfd141a3d5bfdff
ChiHaoLu <b08303113@ntu.edu.tw>
2024-01-24 15:10:53 +0800

    refactor with constrant

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ func main() {
 	defer report.Close()
 	writer := bufio.NewWriter(report)
 	defer writer.Flush()
-	writer.WriteString("# Token Balance\n")
-	writer.WriteString("> Fiat: " + fiat + "\n")
+	writer.WriteString("# Token Balance\n\n")
+	writer.WriteString("> Fiat: " + fiat + "\n\n")
 	writer.WriteString("| Chain | Address | ETH Balance | USDT Balance | USDC Balance | DAI Balance | MATIC Balance | BNB Balance | Value |\n")
 	writer.WriteString("|-------|---------|-------------|--------------|--------------|-------------|---------------|-------------|---------------|\n")
 
